53730e00034a56b69b3e5fd7f317663994e0a778
Vladislavs D <vladislavs.dem@gmail.com>
2023-10-05 16:52:18 +0300

    Worked on menu navigation on menu section (tablet size)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,8 @@
 //* Creating effect when clicking on hamburger [menu] button */
 const menu_btn = document.querySelector('.menu_tablet_hamburger');
+const tablet_menu = document.querySelector('.menu_tablet_navigation');
 
 menu_btn.addEventListener('click', function () {
    menu_btn.classList.toggle('is-active');
+   tablet_menu.classList.toggle('is-active');
 });
