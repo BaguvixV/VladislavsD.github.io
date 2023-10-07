d5ea20ac381816fa66c8c1a6e860cd489a5a261a
Vladislavs D <vladislavs.dem@gmail.com>
2023-10-07 18:02:19 +0300

    Save before using alternative way for hamburger menu navigation js code (tablet size)

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,8 +1,48 @@
 //* Creating effect when clicking on hamburger [menu] button */
+const body = document.querySelector("body"); 
 const menu_btn = document.querySelector('.menu_tablet_hamburger');
-const tablet_menu = document.querySelector('.menu_tablet_navigation');
+const menu_modal = document.querySelector('.menu_tablet_navigation');
 
 menu_btn.addEventListener('click', function () {
+   //* Activates class is-active over menu_tablet_hamburger and sets CSS parametrs to look like "X" to close menu */
    menu_btn.classList.toggle('is-active');
-   tablet_menu.classList.toggle('is-active');
+   //* Activate[to open] menu navigation */
+   menu_modal.classList.toggle('is-active');
 });
+
+
+//* (UNFINISHED) Alternative way [down below]: */
+
+// const showModal = function (e) {
+   //    menu_modal.classList.toggle("hidden");
+   
+   //    if (!menu_modal.classList.contains("hidden")) {
+//          // Disable scroll
+//          body.style.overflow = "hidden";
+//    } else {
+//          // Enable scroll 
+//          body.style.overflow = "auto"; 
+//    }
+// };
+
+
+
+//* (UNFINISHED) Alternative way [down below]: */
+
+// const showDialog = () => {
+//    document.getElementsByClassName('menu_tablet_navigation').classList.add('is-active')
+//    const body = document.body;
+//    const scrollY = body.style.top;
+//    body.style.position = 'fixed';
+// };
+// const closeDialog = () => {
+//    const body = document.body;
+//    const scrollY = body.style.top;
+//    body.style.position = '';
+//    body.style.top = '';
+//    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+//    document.getElementsByClassName('menu_tablet_navigation').classList.remove('is-active');
+// }
+// window.addEventListener('scroll', () => {
+//    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+// });
